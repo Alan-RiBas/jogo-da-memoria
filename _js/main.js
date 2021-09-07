dff6819b05ffa95f9ef833a5947a5b773f7095c2
Alan-RiBas <alanbatista.ribas@gmail.com>
2021-09-07 16:02:48 -0300

    adiciona animação 3d para virar o card

--- _js/main.js
+++ _js/main.js
@@ -1,9 +1,40 @@
 //principáis variáveis
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 //principáis variáveis
 
 function flipCard(){
-    this.classList.toggle('flipped');
+    this.classList.add('flipped');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+    setTimeout(() =>{
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+    },1500)
 }
 
 cards.forEach((card) => {
