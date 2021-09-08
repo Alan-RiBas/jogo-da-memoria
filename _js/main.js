5704204a4e937989cd514870bcffc9560471cbed
Alan-RiBas <alanbatista.ribas@gmail.com>
2021-09-08 12:52:57 -0300

    criar função para nao clicar mais de duas cartas

--- _js/main.js
+++ _js/main.js
@@ -2,9 +2,13 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 //principáis variáveis
 
 function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) return;
+    
     this.classList.add('flipped');
     if(!hasFlippedCard){
         hasFlippedCard = true;
@@ -31,11 +35,17 @@ function disableCards(){
 }
 
 function unflipCards(){
+    lockBoard = true;
+
     setTimeout(() =>{
         firstCard.classList.remove('flipped');
         secondCard.classList.remove('flipped');
+
+        lockboard = false;
     },1500)
 }
+
+
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard);
